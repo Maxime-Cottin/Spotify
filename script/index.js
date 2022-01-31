0e6d743e6702babeb6d79e2a1ceba95118609e59
Maxime <maxime@Maximes-MacBook-Air.local>
2022-01-31 18:35:00 +0100

    WIP work on footer

--- script/index.js
+++ script/index.js
@@ -1,6 +1,18 @@
 window.onload = () => {
+  // Select all list elements in the nav and set the default time transition
+  let listItems = document.querySelectorAll('nav li')
+  let time = 0.05
+  // Set the delay on each element of the list
+  listItems.forEach(item => {
+    item.style.transitionDelay = time+"s";;
+    time += 0.05;
+  });
+  // onClick burger menu
   document.querySelector('#burger').onclick = function() {
+    // Switch between classes on burger menu
     this.classList.toggle('burgerMenu-active')
     this.classList.toggle('burgerMenu')
+    // 
+    document.querySelector('nav').classList.toggle('opened')
   } 
 }
